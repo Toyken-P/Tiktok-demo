75a69bd6d24942ce55e1e140ed676b47c54a3749
yf <178188556@qq.com>
2022-05-18 20:47:52 +0800

    尝试从数据库中读取

---
+++ controller/feed.go
@@ -0,0 +1,37 @@
+package controller
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	"net/http"
+	"time"
+)
+
+type FeedResponse struct {
+	Response
+	VideoList []Video `json:"video_list,omitempty"`
+	NextTime  int64   `json:"next_time,omitempty"`
+}
+
+// Feed same demo video list for every request
+func Feed(c *gin.Context) {
+	latest_time := c.DefaultPostForm("latest_time", time.Now().String())
+	db, err := gorm.Open("mysql", "root:123456@tcp(121.43.190.40:3306)/douyin?charset=utf8&parseTime=True&loc=Local")
+	defer db.Close()
+	if err != nil {
+		fmt.Println("connect db failed : err:", err)
+		return
+	}
+	readvideos := make([]Video, 30)
+	db.Where("created_at < ?", latest_time).Order("created_at desc").Find(&readvideos)
+	fmt.Println("videos", readvideos)
+	next_time := readvideos[len(readvideos)-1].CreatedAt
+	fmt.Println("nexttime:", next_time)
+	c.JSON(http.StatusOK, FeedResponse{
+		Response:  Response{StatusCode: 0},
+		VideoList: readvideos,
+		NextTime:  next_time.Unix(),
+	})
+}
